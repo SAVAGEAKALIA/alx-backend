c5573fc0f8b03460b756bec9dc29fb5b9ee66f8f
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-11-11 18:35:57 +0100

    Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -31,7 +31,7 @@ class LIFOCache(BaseCaching):
                 self.cache_data[key] = item
 
     def get(self, key):
-        """ Return item value"""
+        """ Return item value for item """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
